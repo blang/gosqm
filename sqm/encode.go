26ef141edb2ed9f18b516cd85afddc435de693fd
Benedikt Lang <github@benediktlang.de>
2014-04-23 23:24:37 +0200

    Working encoder

---
+++ sqm/encode.go
@@ -0,0 +1,176 @@
+package sqm
+
+import (
+	"bytes"
+	"io"
+)
+
+const LINEBREAK = "\n\r"
+const INDENT = 2
+
+type Encoder struct {
+	w io.Writer
+}
+
+func NewEncoder(w io.Writer) *Encoder {
+	e := &Encoder{}
+	e.w = w
+	return e
+}
+
+func (e *Encoder) writeString(s string) error {
+	_, err := e.w.Write([]byte(s))
+	return err
+}
+
+func (e *Encoder) Encode(class *Class) error {
+	return e.encodeMainClass(class, 0)
+}
+
+func (e *Encoder) encodeClass(class *Class, level int) error {
+	err := e.writeString(indent(level) + "class " + class.Name + LINEBREAK + indent(level) + "{" + LINEBREAK)
+	if err != nil {
+		return err
+	}
+	err = e.encodeSubElements(class, level+1)
+	if err != nil {
+		return err
+	}
+
+	err = e.writeString(indent(level) + "};" + LINEBREAK)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *Encoder) encodeMainClass(class *Class, level int) error {
+	err := e.encodeSubElements(class, level)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *Encoder) encodeSubElements(class *Class, level int) error {
+	//encode arr properties
+	for _, arrProp := range class.Arrprops {
+		err := e.encodeArrProperty(arrProp, level)
+		if err != nil {
+			return err
+		}
+	}
+
+	//encode properties
+	for _, prop := range class.Props {
+		err := e.encodeProperty(prop, level)
+		if err != nil {
+			return err
+		}
+	}
+
+	//encode subclasses
+	for _, subclass := range class.Classes {
+		err := e.encodeClass(subclass, level)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (e *Encoder) encodeProperty(p *Property, level int) error {
+	var err error
+	switch p.Typ {
+	case TString:
+		err = e.writeString(indent(level) + p.Name + "=\"" + p.Value + "\";" + LINEBREAK)
+	case TInt:
+		fallthrough
+	case TFloat:
+		err = e.writeString(indent(level) + p.Name + "=" + p.Value + ";" + LINEBREAK)
+	}
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *Encoder) encodeArrProperty(arrProp *ArrayProperty, level int) error {
+	if arrProp.Name == "addOns" || arrProp.Name == "addOnsAuto" {
+		return e.encodeAddonsArrProperty(arrProp, level)
+	} else {
+		return e.encodeNormalArrProperty(arrProp, level)
+	}
+}
+
+func (e *Encoder) encodeAddonsArrProperty(arrProp *ArrayProperty, level int) error {
+	err := e.writeString(indent(level) + arrProp.Name + "[]=" + LINEBREAK + indent(level) + "{" + LINEBREAK)
+	if err != nil {
+		return err
+	}
+	for i, val := range arrProp.Values {
+		if i > 0 {
+			err = e.writeString("," + LINEBREAK)
+			if err != nil {
+				return err
+			}
+
+		}
+
+		err = e.writeString(indent(level+1) + "\"" + val + "\"")
+		if err != nil {
+			return err
+		}
+
+	}
+	err = e.writeString(LINEBREAK + indent(level) + "};" + LINEBREAK)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (e *Encoder) encodeNormalArrProperty(arrProp *ArrayProperty, level int) error {
+	err := e.writeString(indent(level) + arrProp.Name + "[]={")
+	if err != nil {
+		return err
+	}
+	for i, val := range arrProp.Values {
+		if i > 0 {
+			err = e.writeString(",")
+			if err != nil {
+				return err
+			}
+
+		}
+		if arrProp.Typ == TString {
+			err = e.writeString("\"" + val + "\"")
+			if err != nil {
+				return err
+			}
+		} else {
+			err := e.writeString(val)
+			if err != nil {
+				return err
+			}
+		}
+	}
+	err = e.writeString("};" + LINEBREAK)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func indent(level int) string {
+	var buffer bytes.Buffer
+
+	for i := 0; i < level*INDENT; i++ {
+		buffer.WriteString(" ")
+	}
+
+	return buffer.String()
+}
---
+++ sqm/encode_test.go
@@ -0,0 +1,316 @@
+package sqm
+
+import (
+	"bytes"
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func newBufEncoder() (*Encoder, *bytes.Buffer) {
+	var e *Encoder
+	var b []byte
+	buf := bytes.NewBuffer(b)
+	e = NewEncoder(buf)
+	return e, buf
+
+}
+
+func TestEncode(t *testing.T) {
+	var e *Encoder
+	var buf *bytes.Buffer
+	Convey("Given a fresh encoder", t, func() {
+		e, buf = newBufEncoder()
+		Convey("Given a Array Property of type String with 3 members", func() {
+			arrProp := &ArrayProperty{
+				Name:   "ArrayProp",
+				Typ:    TString,
+				Values: []string{"first", "second", "third"},
+			}
+			Convey("Encoding it with level 1", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeArrProperty(arrProp, 1)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual, `  ArrayProp[]={"first","second","third"};`+LINEBREAK)
+				})
+			})
+		})
+
+		Convey("Given a Array Property of type Integer with 3 members", func() {
+			arrProp := &ArrayProperty{
+				Name:   "ArrayProp",
+				Typ:    TInt,
+				Values: []string{"1", "2", "3"},
+			}
+			Convey("Encoding it with level 1", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeArrProperty(arrProp, 1)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual, `  ArrayProp[]={1,2,3};`+LINEBREAK)
+				})
+			})
+		})
+
+		Convey("Given a Array Property of type Float with 3 members", func() {
+			arrProp := &ArrayProperty{
+				Name:   "ArrayProp",
+				Typ:    TFloat,
+				Values: []string{"1.123", "2.123", "3.123"},
+			}
+			Convey("Encoding it with level 1", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeArrProperty(arrProp, 1)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual, `  ArrayProp[]={1.123,2.123,3.123};`+LINEBREAK)
+				})
+			})
+		})
+
+		Convey("Given a Array Property of type String with special name 'addOns' with 3 members", func() {
+			arrProp := &ArrayProperty{
+				Name:   "addOns",
+				Typ:    TString,
+				Values: []string{"first", "second", "third"},
+			}
+			Convey("Encoding it with level 1", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeArrProperty(arrProp, 1)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual,
+						`  addOns[]=`+LINEBREAK+
+							`  {`+LINEBREAK+
+							`    "first",`+LINEBREAK+
+							`    "second",`+LINEBREAK+
+							`    "third"`+LINEBREAK+
+							`  };`+LINEBREAK)
+				})
+			})
+		})
+
+		Convey("Given a Array Property of type String with special name 'addOnsAuto' with 3 members", func() {
+			arrProp := &ArrayProperty{
+				Name:   "addOnsAuto",
+				Typ:    TString,
+				Values: []string{"first", "second", "third"},
+			}
+			Convey("Encoding it with level 1", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeArrProperty(arrProp, 1)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual,
+						`  addOnsAuto[]=`+LINEBREAK+
+							`  {`+LINEBREAK+
+							`    "first",`+LINEBREAK+
+							`    "second",`+LINEBREAK+
+							`    "third"`+LINEBREAK+
+							`  };`+LINEBREAK)
+				})
+			})
+		})
+
+		Convey("Given a String Property", func() {
+			prop := &Property{
+				Name:  "key",
+				Typ:   TString,
+				Value: "value",
+			}
+			Convey("Encoding it with level 1", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeProperty(prop, 1)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual, `  key="value";`+LINEBREAK)
+				})
+			})
+		})
+
+		Convey("Given an Integer Property", func() {
+			prop := &Property{
+				Name:  "key",
+				Typ:   TInt,
+				Value: "123",
+			}
+			Convey("Encoding it with level 1", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeProperty(prop, 1)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual, `  key=123;`+LINEBREAK)
+				})
+			})
+		})
+
+		Convey("Given an Float Property", func() {
+			prop := &Property{
+				Name:  "key",
+				Typ:   TFloat,
+				Value: "123.456",
+			}
+			Convey("Encoding it with level 1", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeProperty(prop, 1)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual, `  key=123.456;`+LINEBREAK)
+				})
+			})
+		})
+
+		Convey("Given an emtpy Class", func() {
+			c := &Class{
+				Name: "myclass",
+			}
+			Convey("Encoding it with level 0", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeClass(c, 0)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual, `class myclass`+LINEBREAK+`{`+LINEBREAK+`};`+LINEBREAK)
+				})
+			})
+			Convey("Encoding it with level 1", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeClass(c, 1)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual, `  class myclass`+LINEBREAK+`  {`+LINEBREAK+`  };`+LINEBREAK)
+				})
+			})
+		})
+
+		Convey("Given a Class with an empty subclass", func() {
+			c := &Class{
+				Name: "myclass",
+				Classes: []*Class{
+					&Class{
+						Name: "subclass",
+					},
+				},
+			}
+			Convey("Encoding it with level 0", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeClass(c, 0)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual,
+						`class myclass`+LINEBREAK+
+							`{`+LINEBREAK+
+							`  class subclass`+LINEBREAK+
+							`  {`+LINEBREAK+
+							`  };`+LINEBREAK+
+							`};`+LINEBREAK)
+				})
+			})
+			Convey("Encoding it with level 1", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeClass(c, 1)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual,
+						`  class myclass`+LINEBREAK+
+							`  {`+LINEBREAK+
+							`    class subclass`+LINEBREAK+
+							`    {`+LINEBREAK+
+							`    };`+LINEBREAK+
+							`  };`+LINEBREAK)
+				})
+			})
+		})
+
+		Convey("Given a full Class", func() {
+			c := &Class{
+				Name: "mainclass",
+				Props: []*Property{
+					&Property{
+						Name:  "version",
+						Typ:   TInt,
+						Value: "1",
+					},
+				},
+				Classes: []*Class{
+					&Class{
+						Name: "myclass",
+						Arrprops: []*ArrayProperty{
+							&ArrayProperty{
+								Name:   "addOnsAuto",
+								Typ:    TString,
+								Values: []string{"first", "second", "third"},
+							},
+						},
+						Props: []*Property{
+							&Property{
+								Name:  "key1",
+								Typ:   TString,
+								Value: "value1",
+							},
+							&Property{
+								Name:  "key2",
+								Typ:   TString,
+								Value: "value2",
+							},
+						},
+						Classes: []*Class{
+							&Class{
+								Name: "subclass",
+								Props: []*Property{
+									&Property{
+										Name:  "key1",
+										Typ:   TString,
+										Value: "value1",
+									},
+								},
+							},
+						},
+					},
+				},
+			}
+			Convey("Encoding it with level 0", func() {
+				Convey("Should write correct string", func() {
+					err := e.encodeClass(c, 0)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual,
+						`class mainclass`+LINEBREAK+
+							`{`+LINEBREAK+
+							`  version=1;`+LINEBREAK+
+							`  class myclass`+LINEBREAK+
+							`  {`+LINEBREAK+
+							`    addOnsAuto[]=`+LINEBREAK+
+							`    {`+LINEBREAK+
+							`      "first",`+LINEBREAK+
+							`      "second",`+LINEBREAK+
+							`      "third"`+LINEBREAK+
+							`    };`+LINEBREAK+
+							`    key1="value1";`+LINEBREAK+
+							`    key2="value2";`+LINEBREAK+
+							`    class subclass`+LINEBREAK+
+							`    {`+LINEBREAK+
+							`      key1="value1";`+LINEBREAK+
+							`    };`+LINEBREAK+
+							`  };`+LINEBREAK+
+							`};`+LINEBREAK)
+				})
+			})
+
+			Convey("Encoding it as main class", func() {
+				Convey("Should write correct string", func() {
+					err := e.Encode(c)
+					So(err, ShouldBeNil)
+					So(buf.String(), ShouldEqual,
+						`version=1;`+LINEBREAK+
+							`class myclass`+LINEBREAK+
+							`{`+LINEBREAK+
+							`  addOnsAuto[]=`+LINEBREAK+
+							`  {`+LINEBREAK+
+							`    "first",`+LINEBREAK+
+							`    "second",`+LINEBREAK+
+							`    "third"`+LINEBREAK+
+							`  };`+LINEBREAK+
+							`  key1="value1";`+LINEBREAK+
+							`  key2="value2";`+LINEBREAK+
+							`  class subclass`+LINEBREAK+
+							`  {`+LINEBREAK+
+							`    key1="value1";`+LINEBREAK+
+							`  };`+LINEBREAK+
+							`};`+LINEBREAK)
+				})
+			})
+		})
+	})
+	Convey("Indent should return correct amount of spaces", t, func() {
+		So(indent(0), ShouldEqual, "")
+		So(indent(1), ShouldEqual, "  ")
+		So(indent(2), ShouldEqual, "    ")
+	})
+}
