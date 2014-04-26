744e58e5a539e55f9e41969f1b9900e659f7ea61
Benedikt Lang <github@benediktlang.de>
2014-04-26 18:42:21 +0200

    Further tests for benchmarking

--- mission/encodedecode_test.go
+++ mission/encodedecode_test.go
@@ -62,6 +62,56 @@ func BenchmarkFullEncode(b *testing.B) {
 	}
 }
 
+func BenchmarkMissionEncode(b *testing.B) {
+	buf, err := ioutil.ReadFile("../sqm/mission.sqm")
+	bufstr := string(buf)
+	if err != nil {
+		b.Errorf("Could not open mission.sqm")
+		return
+	}
+	p := sqm.MakeParser(bufstr)
+	// c, perr := p.Run()
+	class, perr := p.Run()
+	if perr != nil {
+		b.Errorf("Parser returned with error %q", perr)
+	}
+	mp := NewParser()
+	missionFile, err := mp.Parse(class)
+	if err != nil {
+		b.Errorf("Can't parse class to missionfile, %q", err)
+	}
+
+	for n := 0; n < b.N; n++ {
+		enc := NewEncoder()
+		enc.Encode(missionFile)
+	}
+}
+
+func BenchmarkMissionDecode(b *testing.B) {
+	buf, err := ioutil.ReadFile("../sqm/mission.sqm")
+	bufstr := string(buf)
+	if err != nil {
+		b.Errorf("Could not open mission.sqm")
+		return
+	}
+	p := sqm.MakeParser(bufstr)
+	// c, perr := p.Run()
+	class, perr := p.Run()
+	if perr != nil {
+		b.Errorf("Parser returned with error %q", perr)
+	}
+	for n := 0; n < b.N; n++ {
+		mp := NewParser()
+		missionFile, err := mp.Parse(class)
+		if err != nil {
+			b.Errorf("Can't parse class to missionfile, %q", err)
+		}
+		if len(missionFile.Mission.Groups) < 5 {
+			b.Errorf("Error while parsing mission, %q", missionFile)
+		}
+	}
+}
+
 func BenchmarkFullEncodeDecode(b *testing.B) {
 	buf, err := ioutil.ReadFile("../sqm/mission.sqm")
 	bufstr := string(buf)
@@ -83,13 +133,42 @@ func BenchmarkFullEncodeDecode(b *testing.B) {
 		}
 		enc := NewEncoder()
 		eclass := enc.Encode(missionFile)
-		var buf []byte
-		buffer := bytes.NewBuffer(buf)
+		var ebuf []byte
+		buffer := bytes.NewBuffer(ebuf)
 		sqmenc := sqm.NewEncoder(buffer)
 		err = sqmenc.Encode(eclass)
 		if err != nil {
 			b.Errorf("Can't encode class, %q", err)
 		}
 
+	}
+}
+
+func TestFullEncodeDecode(t *testing.T) {
+	buf, err := ioutil.ReadFile("../sqm/mission.sqm")
+	bufstr := string(buf)
+	if err != nil {
+		t.Errorf("Could not open mission.sqm")
+		return
+	}
+	p := sqm.MakeParser(bufstr)
+	// c, perr := p.Run()
+	class, perr := p.Run()
+	if perr != nil {
+		t.Errorf("Parser returned with error %q", perr)
+	}
+	mp := NewParser()
+	missionFile, err := mp.Parse(class)
+	if err != nil {
+		t.Errorf("Can't parse class to missionfile, %q", err)
+	}
+	enc := NewEncoder()
+	eclass := enc.Encode(missionFile)
+	var ebuf []byte
+	buffer := bytes.NewBuffer(ebuf)
+	sqmenc := sqm.NewEncoder(buffer)
+	err = sqmenc.Encode(eclass)
+	if err != nil {
+		t.Errorf("Can't encode class, %q", err)
 	}
 }
